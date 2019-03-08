4ed6e18b4b3d635aa2db0ab2d5f2aa7d79921442
Radoslaw Medryk <radoslaw.medryk@yhoo.com>
2019-03-08 20:27:40 +0100

    Added TODO comments

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export * from "./Processor";
 export * from "./postgres";
 
 // TODO [RM]: write unit tests
+// TODO [RM]: support for indexes
+// TODO [RM]: separate table declarations from foreign key constraints to avoid table creating order issues
